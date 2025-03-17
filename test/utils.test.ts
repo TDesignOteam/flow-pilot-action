d6488f80dbf8f7a012360e0ae33cf5f3b397320f
黎伟杰 <674416404@qq.com>
2025-03-17 21:36:21 +0800

    test: isExtractPRLog

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,7 +1,8 @@
 import type { Tokens } from 'marked'
 import { readFileSync } from 'node:fs'
 import { describe, expect, it } from 'vitest'
-import { extractChangelog, parseMarkdown, renderPackages } from '../src/utils'
+import { pull_request_data } from '../fixtures/pull_request_data'
+import { extractChangelog, getPackages, isExtractPRLog, parseMarkdown } from '../src/utils'
 
 describe('utils', () => {
   it('parseMarkdown', () => {
@@ -13,18 +14,51 @@ describe('utils', () => {
     expect(data.text).toBe('📝 更新日志')
   })
 
-  it('renderPackages', () => {
-    const packages = renderPackages('fixtures/repo1')
+  it('getPackages', () => {
+    const packages = getPackages('fixtures/repo1')
     expect(packages.length).toBe(2)
     expect(packages[0].packageJson.name).toBe('pkg-a')
     expect(packages[0].relativeDir).toBe('packages/pkg-a')
     expect(packages[1].packageJson.name).toBe('pkg-c')
     expect(packages[1].relativeDir).toBe('packages/pkg-c')
   })
+  describe('isExtractPRLog', () => {
+    it('labels 包含 skip-changelog', () => {
+      const prData = { ...pull_request_data }
+      prData.labels = [{
+        id: 1,
+        node_id: '123',
+        name: 'skip-changelog',
+        url: 'q3',
+        color: '123',
+        default: true,
+        description: '123',
+      }]
+      expect(isExtractPRLog(prData)).toBe(false)
+    })
+    it('发布版本PR，release/ 开头', () => {
+      const prData = { ...pull_request_data }
+      prData.head.ref = 'release/1.0.0'
+      expect(isExtractPRLog(prData)).toBe(false)
+    })
+    it('pr body 申明不纳入 Changelog', () => {
+      const prData = { ...pull_request_data }
+      prData.body = '### 📝 更新日志 \n\n - [x] 本条 PR 不需要纳入 Changelog'
+      expect(isExtractPRLog(prData)).toBe(false)
+    })
+    it('pr user type 是 Bot', () => {
+      const prData = { ...pull_request_data }
+      prData.user.type = 'Bot'
+      expect(isExtractPRLog(prData)).toBe(false)
+    })
+    it('正常PR 数据', () => {
+      expect(isExtractPRLog(pull_request_data)).toBe(true)
+    })
+  })
 
   describe('extractChangelog', () => {
     it('repo1 pkg-b是私有包，不收集日志', () => {
-      const packages = renderPackages('fixtures/repo1')
+      const packages = getPackages('fixtures/repo1')
       expect(packages.length).toBe(2)
       expect(packages[0].relativeDir).toBe('packages/pkg-a')
       expect(packages[1].relativeDir).toBe('packages/pkg-c')
@@ -32,8 +66,9 @@ describe('utils', () => {
       const log = extractChangelog(body, packages.map(pkg => pkg.packageJson.name))
       expect(log).toMatchSnapshot()
     })
+
     it('repo2 无私有包', () => {
-      const packages = renderPackages('fixtures/repo2')
+      const packages = getPackages('fixtures/repo2')
       expect(packages.length).toBe(3)
       expect(packages[0].relativeDir).toBe('packages/pkg-a')
       expect(packages[1].relativeDir).toBe('packages/pkg-b')
@@ -42,8 +77,9 @@ describe('utils', () => {
       const log = extractChangelog(body, packages.map(pkg => pkg.packageJson.name))
       expect(log).toMatchSnapshot()
     })
+
     it('本条 PR 不需要纳入 Changelog', () => {
-      const packages = renderPackages('fixtures/repo1')
+      const packages = getPackages('fixtures/repo1')
 
       const body = readFileSync('fixtures/pull_request_body/pr_body2.md', 'utf8').replaceAll('\n', '\r\n')
       const log = extractChangelog(body, packages.map(pkg => pkg.packageJson.name))
