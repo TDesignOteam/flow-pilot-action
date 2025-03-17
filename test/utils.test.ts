e92c917a3c07341d4b997b56f69f1fcfd99a9891
黎伟杰 <674416404@qq.com>
2025-03-17 21:42:21 +0800

    chore: fix test

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,5 @@
 import type { Tokens } from 'marked'
+import type { PullRequestData } from '../src/types'
 import { readFileSync } from 'node:fs'
 import { describe, expect, it } from 'vitest'
 import { pull_request_data } from '../fixtures/pull_request_data'
@@ -24,7 +25,7 @@ describe('utils', () => {
   })
   describe('isExtractPRLog', () => {
     it('labels 包含 skip-changelog', () => {
-      const prData = { ...pull_request_data }
+      const prData: PullRequestData = JSON.parse(JSON.stringify(pull_request_data))
       prData.labels = [{
         id: 1,
         node_id: '123',
@@ -37,17 +38,17 @@ describe('utils', () => {
       expect(isExtractPRLog(prData)).toBe(false)
     })
     it('发布版本PR，release/ 开头', () => {
-      const prData = { ...pull_request_data }
+      const prData: PullRequestData = JSON.parse(JSON.stringify(pull_request_data))
       prData.head.ref = 'release/1.0.0'
       expect(isExtractPRLog(prData)).toBe(false)
     })
     it('pr body 申明不纳入 Changelog', () => {
-      const prData = { ...pull_request_data }
+      const prData: PullRequestData = JSON.parse(JSON.stringify(pull_request_data))
       prData.body = '### 📝 更新日志 \n\n - [x] 本条 PR 不需要纳入 Changelog'
       expect(isExtractPRLog(prData)).toBe(false)
     })
     it('pr user type 是 Bot', () => {
-      const prData = { ...pull_request_data }
+      const prData: PullRequestData = JSON.parse(JSON.stringify(pull_request_data))
       prData.user.type = 'Bot'
       expect(isExtractPRLog(prData)).toBe(false)
     })
