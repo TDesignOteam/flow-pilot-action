745086b2bb3ca8999b98a330d01858a06712c754
黎伟杰 <674416404@qq.com>
2025-03-18 15:33:27 +0800

    test: OLD_VERSION_REG & NEW_VERSION_REG

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,3 @@
 export const SKIP_CHANGELOG_REG = /\[x\] 本条 PR 不需要纳入 Changelog/i
+export const OLD_VERSION_REG = /\s*-\s*"version":\s*"(.*)"/
+export const NEW_VERSION_REG = /\s*\+\s*"version":\s*"(.*)"/
