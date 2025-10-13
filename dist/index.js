f2da3315c1a01abc032131309ceaa9bae6b5c8d4
黎伟杰 <674416404@qq.com>
2025-10-13 17:09:44 +0800

    test

--- dist/index.js
+++ dist/index.js
@@ -59619,8 +59619,8 @@ function pull_request(token) {
                     const day = String(currentDate.getDate()).padStart(2, '0');
                     // 中文日志
                     yield addComment(prNumber, `${logHead}# 🎉 发布 ${changelogs.pkg}\n## 🌈 ${changelogs.version} \`${year}-${month}-${day}\` \n\n${md}`);
-                    const secretId = (0, core_1.getInput)('tmt-secret-id');
-                    const secretKey = (0, core_1.getInput)('tmt-secret-key');
+                    const secretId = (0, core_1.getInput)('tmt-secret-id', { trimWhitespace: true });
+                    const secretKey = (0, core_1.getInput)('tmt-secret-key', { trimWhitespace: true });
                     (0, core_1.info)(`secretId: ${secretId}`);
                     (0, core_1.info)(`secretKey: ${secretKey}`);
                     if (secretId && secretKey) {
